200c2c4209a991662e5185196c8f5572c587976d
Flore Laforge <flore.laforge@westpac.com.au>
2019-04-26 13:05:49 +1000

    Updated apiURL in config. Add package-lock.json on to gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
         resolve: `gatsby-source-strapi`,
         options: {
-            apiURL: `http://localhost:1337`,
+            apiURL: process.env.DEPLOY_URL
+                ? 'https://immense-gorge-43384.herokuapp.com'
+                : 'http://localhost:1337',
             contentTypes: [
                 `article`,
                 `user`
